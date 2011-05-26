59a9acfa5f6074dd9b544b50c8cd8e0fec99f68c
Bruce Phillips <github@fastmailbox.net>
2011-05-25 21:10:49 -0400

    Remove at-the-door.php.

--- registration/at-the-door.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-
-<p><?php printf(__('Preregistration for this event end on: %s', 'nsevent'), $event->get_date_prereg_end('c')); ?></p>
-
-<p>Check out at-the-door prices for individual dances on the <a href="/venues/">Venues</a> page.</p>
-
-<?php get_footer(); ?>
