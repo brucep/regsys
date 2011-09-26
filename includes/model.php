11bdc65b439044da2e516292ba8947c207ea56f4
Bruce Phillips <github@fastmailbox.net>
2011-09-25 22:47:05 -0400

    Remove obsolete method from NSEvent_Model.

--- includes/model.php
+++ includes/model.php
@@ -13,36 +13,4 @@ abstract class NSEvent_Model
 	{
 		self::$options = $options;
 	}
-		
-	static protected function bit_field($input, array $fields, $return = '')
-	{
-		$result = array();
-		$result_booleans = array();
-		krsort($fields);
-		
-		foreach ($fields as $key => $value) {
-			// When not using validated data, check if input is too large
-			if ($input >= $key) {
-				$result[$key] = $value;
-				$result_booleans[$value] = true;
-				$input -= $key;
-			}
-			else {
-				$result_booleans[$value] = false;
-			}
-		}
-		
-		ksort($result);
-		ksort($result_booleans);
-		
-		if ($return == 'string') {
-			return ucwords(implode(', ', array_keys(array_filter($result_booleans))));
-		}
-		elseif ($return == 'booleans') {
-			return $result_booleans;
-		}
-		else {
-			return $result;
-		}
-	}
 }
