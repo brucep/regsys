4e41a40a1ab3225f92733fd7fddd12a0bb0e6ad6
Bruce Phillips <github@fastmailbox.net>
2012-01-06 21:52:18 -0500

    Update support for deleting dancers.

--- includes/request-controller.php
+++ includes/request-controller.php
@@ -2,6 +2,29 @@
 
 class NSEvent_Request_Controller
 {
+	static public function admin_dancer_delete($event, $dancer)
+	{
+		if (isset($_POST['confirmed'])) {
+			$database = NSEvent::get_database_connection();
+			
+			$database->query('DELETE FROM %1$s_registrations WHERE event_id = ? AND dancer_id = ?',         array($event->id(), $dancer->id()));
+			$database->query('DELETE FROM %1$s_housing       WHERE event_id = ? AND dancer_id = ? LIMIT 1', array($event->id(), $dancer->id()));
+			$database->query('DELETE FROM %1$s_dancers       WHERE event_id = ? AND dancer_id = ? LIMIT 1', array($event->id(), $dancer->id()));
+			
+			wp_redirect(site_url('wp-admin/admin.php') . sprintf('?page=nsevent&request=report_index_event&event_id=%d&deleted_dancer=%s', $event->id(), rawurlencode($dancer->name())));
+			exit();
+		}
+		
+		# Needed if the confirmation checkbox wasn't checked.
+		if (isset($_GET['noheader'])) {
+			require_once ABSPATH . 'wp-admin/admin-header.php';
+		}
+		
+		echo NSEvent::render_template('admin/dancer-delete.html', array(
+			'event'  => $event,
+			'dancer' => $dancer));
+	}
+	
 	static public function admin_dancer_resend_confirmation_email($event, $dancer)
 	{
 		$dancer->send_confirmation_email();
