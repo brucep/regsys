e79b934ba5140866d961f476236d12e3d9c35739
Bruce Phillips <github@fastmailbox.net>
2012-05-02 14:49:10 -0400

    Add Discounts report.

--- includes/request-controller.php
+++ includes/request-controller.php
@@ -329,6 +329,17 @@ class RegistrationSystem_Request_Controller
 			'dancers' => $event->dancers()));
 	}
 	
+	static public function report_discounts($event)
+	{
+		$database = RegistrationSystem::get_database_connection();
+		
+		$dancers = $database->query('SELECT * FROM %s_dancers WHERE event_id = ? AND discount_id IS NOT NULL ORDER BY discount_id ASC, last_name ASC, first_name ASC', array($event->id()))->fetchAll(PDO::FETCH_CLASS, 'RegistrationSystem_Model_Dancer');
+		
+		echo RegistrationSystem::render_template('reports/discounts.html', array(
+			'event'   => $event,
+			'dancers' => $dancers));
+	}
+	
 	static public function report_download_csv($event)
 	{
 		if (!isset($_GET['data']) or !in_array($_GET['data'], array('competitions', 'dancers', 'housing_needed', 'housing_providers', 'volunteers'))) {
