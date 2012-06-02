0a0f75fa4e421f7f0bdef8cbc7e2bf84079c9214
Bruce Phillips <github@fastmailbox.net>
2012-06-02 11:58:38 -0400

    Add visualization chart for registration numbers.

--- includes/request-controller.php
+++ includes/request-controller.php
@@ -537,7 +537,57 @@ class RegistrationSystem_Request_Controller
 	
 	static public function report_index_visualization()
 	{
-		echo RegistrationSystem::render_template('reports/index-visualization.html');
+		$database = RegistrationSystem::get_database_connection();
+		$options  = RegistrationSystem::get_options();
+		$events   = array_reverse(RegistrationSystem_Model_Event::get_events());
+		$data     = array(0 => array('x'));
+		
+		foreach ($events as $event) {
+			$data[0][] = $event->name;
+		}
+		
+		// Modified from http://boonedocks.net/mike/archives/137-Creating-a-Date-Range-Array-with-PHP.html
+		function createDateRange($start, $end) {
+			$range = array();
+			$start = strtotime($start);	
+			$end   = strtotime($end);
+			
+			if ($start > $end) {
+				return createDateRangeArray($end, $start);
+			}
+			
+			while($start <= $end) {
+				$range[] = date('m-d', $start);
+				$start = strtotime('+ 1 day', $start);
+			}
+			
+			return $range;
+		}
+		
+		$dates = $database->query('SELECT MIN(DISTINCT FROM_UNIXTIME(date_registered, "%%m-%%d")) AS start, MAX(DISTINCT FROM_UNIXTIME(date_registered, "%%m-%%d")) AS end FROM %1$s_dancers WHERE date_registered > 1')->fetch(PDO::FETCH_OBJ);
+		$dates = createDateRange('2012-' . $dates->start, '2012-' . $dates->end);
+		
+		$i = 1;
+		foreach ($dates as $date) {
+			$data[$i] = array($date);
+			
+			foreach ($events as $event) {
+				if ($event->id() == $options['current_event_id'] and strtotime(date('Y-') . $date) > time()) {
+					$data[$i][] = null;
+				}
+				elseif ($date > date('m-d', $event->date_paypal_prereg_end())) {
+					$data[$i][] = null;
+				}
+				else {
+					$count = (int) $database->query('SELECT COUNT(dancer_id) FROM %1$s_dancers WHERE FROM_UNIXTIME(date_registered, "%%m-%%d") <= ? AND event_id = ?', array($date, $event->id()))->fetchColumn();
+					$data[$i][] = $count > 0 ? $count : null;
+				}
+			}
+			
+			$i++;
+		}
+		
+		echo RegistrationSystem::render_template('reports/index-visualization.html', array('json' => json_encode($data)));
 	}
 	
 	static public function report_housing_needed($event)
